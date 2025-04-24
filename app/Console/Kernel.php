e258d4eae238e2100508337e7a8583af49579e7b
Clément Chevalier <clementchevalier2102@outlook.fr>
2025-04-24 15:51:36 +0200

    Delete Kernel.php

--- app/Console/Kernel.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Ce fichier n'est pas utilisé dans Laravel 12
-// La configuration des commandes se trouve dans bootstrap/app.php et routes/console.php
-// Vous pouvez supprimer ce fichier en toute sécurité
